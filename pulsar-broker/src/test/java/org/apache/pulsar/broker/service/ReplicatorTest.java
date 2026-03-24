cfe97e223ad7e965804f38791b51bb7eacec1d5a
gosonzhang <gosonzhang@tencent.com>
2026-03-24 15:35:54 +0800

    fix

--- pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ReplicatorTest.java
+++ pulsar-broker/src/test/java/org/apache/pulsar/broker/service/ReplicatorTest.java
@@ -44,8 +44,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
